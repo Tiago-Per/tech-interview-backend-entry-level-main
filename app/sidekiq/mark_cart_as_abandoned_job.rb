b4577ff8d80e8ea1588a5f5f0fd2fbd743520fae
Tiago Pereira <tiagojunior769@gmail.com>
2025-10-31 23:22:27 -0300

    feat(cart): add Sidekiq job to manage abandoned carts
    - Created MarkCartAsAbandonedJob to mark carts inactive for 3+ hours as abandoned
    - Automatically removes carts abandoned for more than 7 days
    - Integrated with Sidekiq and sidekiq-scheduler for periodic execution

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -1,7 +1,36 @@
 class MarkCartAsAbandonedJob
   include Sidekiq::Job
+  
+  def perform
+    mark_abandoned_carts
+    remove_abandoned_carts
+  end
 
-  def perform(*args)
-    # TODO Impletemente um Job para gerenciar, marcar como abandonado. E remover carrinhos sem interação. 
+  private
+
+  def mark_abandoned_carts
+    threshold = 3.hours.ago
+
+    carts_to_mark = Cart
+      .where(abandoned: false)
+      .where('updated_at < ?', threshold)
+
+    carts_to_mark.find_each do |cart|
+      cart.update!(abandoned: true)
+      Rails.logger.info("Cart ##{cart.id} marked as abandoned")
+    end
+  end
+
+  def remove_abandoned_carts
+    threshold = 7.days.ago
+
+    carts_to_delete = Cart
+      .where(abandoned: true)
+      .where('updated_at < ?', threshold)
+
+    carts_to_delete.find_each do |cart|
+      cart.destroy!
+      Rails.logger.info("Cart ##{cart.id} removed after 7 days abandoned")
+    end
   end
 end
---
+++ db/migrate/20251101014344_add_abandoned_fields_to_carts.rb
@@ -0,0 +1,5 @@
+class AddAbandonedFieldsToCarts < ActiveRecord::Migration[7.1]
+  def change
+    add_column :carts, :abandoned, :boolean
+  end
+end
